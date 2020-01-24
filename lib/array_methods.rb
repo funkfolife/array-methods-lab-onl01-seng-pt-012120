462500c12bbc1b383ba030d0bdb04d1d854d1f7f
jonathan e godinez <jongodinezbass@yahoo.com>
2020-01-24 19:11:06 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -1,8 +1,8 @@
 def using_include(array, element)
    array = ["wow", "I", "am", "really", "learning", "arrays!"]
-   element1 = "wow"
+   #element1 = "wow"
    array.include?("wow")
-   element2 = "Tardis"
+   #element2 = "Tardis"
    array.include?("Tardis")
 end
 
